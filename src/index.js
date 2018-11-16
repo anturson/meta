821dff4bb3f603adf647f1ad6ca113577f883cf0
Artur Nizamov <artur.nizamov@project-plato.com>
2018-11-16 17:14:34 +0300

    express router

--- src/index.js
+++ src/index.js
@@ -7,16 +7,21 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 app.set('port', port);
 
+const router = express.Router();
+
+router.route('/users')
+  .get((req, res) => {
+    res.json([
+      { id: 1, firstName: "Bob", lastName: "Smith", email: "bob@gmail.com" },
+      { id: 2, firstName: "Tammy", lastName: "Norton", email: "tammy@gmail.com" },
+      { id: 3, firstName: "Tina", lastName: "Lee", email: "tina@gmail.com" },
+    ]);
+  });
+
+app.use('/api', router);
+
 app.get('/', (req, res) => {
   res.send('Hello');
-});
-
-app.get('/users', (req, res) => {
-  res.json([
-    { id: 1, firstName: "Bob", lastName: "Smith", email: "bob@gmail.com" },
-    { id: 2, firstName: "Tammy", lastName: "Norton", email: "tammy@gmail.com" },
-    { id: 3, firstName: "Tina", lastName: "Lee", email: "tina@gmail.com" },
-  ]);
 });
 
 app.listen(app.get('port'), () => {
